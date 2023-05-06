971a125fda00dc017e724943e5c7e5ca0107964c
victor039 <tor-vic@hotmail.com>
2023-05-05 23:17:55 -0500

    uploading file persistencia de datos

---
+++ persistencia/index.js
@@ -0,0 +1,14 @@
+const nombre = "victor"
+const apellido = "Vergara"
+
+const datos = {
+    nombre,
+    apellido
+}
+
+ //sessionStorage.setItem("datos", JSON.stringify(datos))
+
+// localStorage.setItem("datos", JSON.stringify(datos))
+
+const now = new Date()
+//document.cookie = `datos=${JSON.stringify(datos)};expires=${new Date(now.getTime() + 2 * 60000)}`
